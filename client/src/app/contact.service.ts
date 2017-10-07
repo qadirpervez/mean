5b43d5d2cdf98a85363da22f47c6c44407507267
Qadir Pervez <786.qadir.pervez@gmail.com>
2017-10-08 00:35:11 +0530

    Done wow

---
+++ client/src/app/contact.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {Http, Headers} from '@angular/http';
+import {Contact} from './contact';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class ContactService {
+
+  constructor(private http: Http) { }
+
+  //retrieve Contact
+  getContacts(){
+    return this.http.get('http://localhost:3000/api/contacts').map(res => res.json());
+  }
+
+  //Add contact method
+  addContact(newContact){
+    var headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return this.http.post('http://localhost:3000/api/contact', newContact, {headers: headers}).map(res => res.json());
+  }
+
+  //Delete Contact
+  deleteContact(id){
+    return this.http.delete('http://localhost:3000/api/contact/'+id).map(res => res.json());
+  }
+}
